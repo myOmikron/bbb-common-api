b057eae6ba9914c9620c6574e8438b0baeff741d
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-04-02 02:28:12 +0200

    Implemented a get version

--- views.py
+++ views.py
@@ -7,6 +7,44 @@ from rc_protocol import validate_checksum
 from django.conf import settings
 
 
+class GetApiPoint(View):
+
+    required_parameters: list
+    endpoint: str
+
+    def get(self, request, *args, **kwargs):
+        # Validate checksum
+        try:
+            if not validate_checksum(request.GET.dict(), settings.SHARED_SECRET,
+                                     self.endpoint, settings.SHARED_SECRET_TIME_DELTA):
+                return JsonResponse(
+                    {"success": False, "message": "Checksum was incorrect."},
+                    status=400,
+                    reason="Checksum was incorrect."
+                )
+        except ValueError:
+            return JsonResponse(
+                {"success": False, "message": "No checksum was given."},
+                status=400,
+                reason="No checksum was given."
+            )
+
+        # Check required parameters
+        for param in self.required_parameters:
+            if param not in request.GET:
+                return JsonResponse(
+                    {"success": False, "message": f"Parameter {param} is mandatory but missing."},
+                    status=400,
+                    reason=f"Parameter {param} is mandatory but missing."
+                )
+
+        # Hand over to subclass
+        return self.safe_get(request, *args, **kwargs)
+
+    def safe_get(self, request, *args, **kwargs):
+        return NotImplemented
+
+
 class PostApiPoint(View):
 
     required_parameters: list
